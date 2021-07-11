5e5eaefdd2360a9051e40e8ab3e99a9f4dbec47c
abad89 <abadalamenti89@gmail.com>
2021-07-11 10:33:34 -0400

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,29 @@
 # Add your code here
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        puts @@all.map{ |dog| dog.name}
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+    
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+end
